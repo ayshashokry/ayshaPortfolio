942201451d5de055dd9c9a3c0d4b95ca8b9ed26a
Aysha-Shokry <aishashokry58@gmail.com>
2023-01-17 20:25:55 +0200

    finalize35

--- components/ProjectDetails.js
+++ components/ProjectDetails.js
@@ -13,12 +13,23 @@ export default function ProjectDetails({ website, index }) {
         <img
           alt="Project's Photo"
           className={styles.projectImg}
-          src={website.img}width="100%"
+          src={website.img}
+          width="100%"
         />
       </Col>
       <Col sm={{ span: 24 }} lg={{ span: 12 }}>
         <h3>{website.name}</h3>
         <p>{website.data}</p>
+        {website.userData ? (
+          <>
+            <h5 className={styles.role}>Login data :</h5>
+            <h4 className={styles.roleSpan}>
+              <strong className={styles.role1}>UserName:</strong> {website.userData.username}<br/>
+              <strong className={styles.role1}>Password: </strong>
+              {website.userData.password}
+            </h4>
+          </>
+        ) : null}
         <div className={styles.linksDiv}>
           <a target="_blank" rel="noreferrer" href={website.demo}>
             <Button>
@@ -35,6 +46,9 @@ export default function ProjectDetails({ website, index }) {
             </a>
           )}
         </div>
+
+        <h5 className={styles.role}>Role :</h5>
+        <h4 className={styles.roleSpan}>{website.role}</h4>
       </Col>
     </Row>
   );
